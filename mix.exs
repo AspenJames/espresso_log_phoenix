5872f5da0e50ed98a0df49fd8b642340970191bc
Aspen James <aspenjames@tqca.org>
2018-11-05 16:24:40 -0800

    add Guardian

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule EspressoLog.Mixfile do
       {:cowboy, "~> 1.0"},
       {:plug_cowboy, "~> 1.0"},
       {:comeonin, "~> 4.0"},
-      {:bcrypt_elixir, "~> 1.0"}
+      {:bcrypt_elixir, "~> 1.0"},
+      {:guardian, "~> 1.0"}
     ]
   end
 
