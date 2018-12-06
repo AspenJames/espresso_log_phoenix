791bbd1b4f05993332e98021a31f1f64debcb528
Aspen James <aspenjames@tqca.org>
2018-12-06 07:44:55 -0800

    work on cafe#create

--- lib/espresso_log_web/controllers/cafe_controller.ex
+++ lib/espresso_log_web/controllers/cafe_controller.ex
@@ -12,12 +12,17 @@ defmodule EspressoLogWeb.CafeController do
   end
 
   def create(conn, %{"cafe" => cafe_params}) do
-    with {:ok, %Cafe{} = cafe} <- Accounts.create_cafe(cafe_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", cafe_path(conn, :show, cafe))
-      |> render("show.json", cafe: cafe)
-    end
+    cafe = %Cafe{}
+      |> Cafe.changeset(cafe_params)
+      |> Repo.insert()
+
+    IO.inspect cafe
+    # with {:ok, %Cafe{} = cafe} <- Accounts.create_cafe(cafe_params) do
+    #   conn
+    #   |> put_status(:created)
+    #   |> put_resp_header("location", cafe_path(conn, :show, cafe))
+    #   |> render("show.json", cafe: cafe)
+    # end
   end
 
   def show(conn, %{"id" => id}) do
